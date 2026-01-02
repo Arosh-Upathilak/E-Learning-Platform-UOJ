7ae111ec198a79f61c2670f3f3f43d2e5ffbb2e4
Arosh-Upathilak <aroshupathilak@gmail.com>
2026-01-03 01:25:36 +0530

    fixed the backend cors origin issue

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ import fileRouter from './routes/fileRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 5000;
-const frontend_url = process.env.FRONTEND_URL ;
+const frontend_url = process.env.FRONTEND_URL_DEVELOPMENT_URL ;
 
 //middleware
 /*
@@ -25,7 +25,7 @@ app.use(cors({
 app.use(
   cors({
     origin: [
-      "https://e-learning-platform-uoj.netlify.app",
+      frontend_url,
       "http://localhost:3000", // for local dev
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
