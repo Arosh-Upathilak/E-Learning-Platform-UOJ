f9d3879b000224e3baf4bf9ca2baa1ee9b1a80e8
Arosh-Upathilak <aroshupathilak@gmail.com>
2026-01-03 13:55:46 +0530

    fixed the frontend  issue

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect, useCallback } from "react";
+import React, { createContext, useState, useEffect } from "react";
 import { toast } from "sonner";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
